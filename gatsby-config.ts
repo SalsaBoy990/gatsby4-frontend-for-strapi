f3850d2d2bf80c52dd1b4054c603d03a0e92272e
Gulácsi András <guland@protonmail.com>
2022-08-26 04:03:07 +0200

    fix: Disable webp image generation by image sharp plugin
    Reason: Netlify build fails because the image generations use too
    much resources -> disabling webp generation lowers resource usage.

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -58,7 +58,24 @@ module.exports = {
     },
     `gatsby-plugin-image`,
     `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-sharp`,
+      options: {
+        defaults: {
+          formats: [`auto`],
+          placeholder: `dominantColor`,
+          quality: 50,
+          breakpoints: [750, 1080, 1366, 1920],
+          backgroundColor: `transparent`,
+          tracedSVGOptions: {},
+          blurredOptions: {},
+          jpgOptions: {},
+          pngOptions: {},
+          webpOptions: {},
+          avifOptions: {},
+        },
+      },
+    },
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
